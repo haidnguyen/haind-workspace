4698ce4ed3dd99a53000aeebe5ceea4e45705046
haidnguyen <dinhhai281@gmail.com>
2022-07-28 12:57:16 +0700

    feat(home): navigation menu highlight

--- libs/blog/ui-layout/src/lib/navigation-menu.tsx
+++ libs/blog/ui-layout/src/lib/navigation-menu.tsx
@@ -1,4 +1,4 @@
-import { VStack, Link as ChLink, Box } from '@chakra-ui/react';
+import { VStack, Link as ChLink, Box, Flex } from '@chakra-ui/react';
 import Link from 'next/link';
 
 export interface NavigationMenuProps {
@@ -10,12 +10,28 @@ export function NavigationMenu({ onLinkClick, routes }: NavigationMenuProps) {
   return (
     <VStack spacing={[3, 1]} align='stretch' as='ul'>
       {routes.map(({ emoji, path, label }) => (
-        <ChLink key={path} as='li' listStyleType='none' onClick={onLinkClick} p={1}>
-          <Box as='span' role='img' mr={2} fontSize='xl'>
+        <Flex
+          key={path}
+          as='li'
+          listStyleType='none'
+          onClick={onLinkClick}
+          p={1.5}
+          borderRadius={4}
+          px={3}
+          align='center'
+          _hover={{
+            bgColor: {
+              sm: 'red.100',
+            },
+          }}
+        >
+          <Box as='span' role='img' mr={4} fontSize='xl'>
             {emoji}
           </Box>
-          <Link href={path}>{label}</Link>
-        </ChLink>
+          <Link href={path}>
+            <ChLink w='100%'>{label}</ChLink>
+          </Link>
+        </Flex>
       ))}
     </VStack>
   );
