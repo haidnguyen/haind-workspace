49499a3d417cd8572a3b08f8cebe7ac2ddc2e845
haidnguyen <dinhhai281@gmail.com>
2022-08-02 22:28:44 +0700

    feat(post): add syntax highlighter

--- libs/blog/feature-post/src/lib/blog-feature-post.tsx
+++ libs/blog/feature-post/src/lib/blog-feature-post.tsx
@@ -2,8 +2,10 @@ import { PostDetail } from '@haind-workspace/blog/data-posts';
 import { useTitle } from '@haind-workspace/blog/data-title';
 import { Date } from '@haind-workspace/blog/ui-date';
 import { Layout } from '@haind-workspace/blog/ui-layout';
-import ReactMarkdown from 'react-markdown';
 import Head from 'next/head';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export interface BlogFeaturePostProps {
   postData: PostDetail;
@@ -24,7 +26,28 @@ export function BlogFeaturePost({ postData }: BlogFeaturePostProps) {
       <br />
       <Date dateString={postData.date} />
       <br />
-      <ReactMarkdown>{postData.content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={okaidia as any}
+                PreTag='div'
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {postData.content}
+      </ReactMarkdown>
     </Layout>
   );
 }
