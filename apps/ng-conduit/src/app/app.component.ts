05850a1cb56561de7ba34e91e309e8888d2ed11f
haidnguyen <dinhhai281@gmail.com>
2022-08-14 22:08:07 +0700

    chore: add ng-conduit application

---
+++ apps/ng-conduit/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'haind-workspace-root',
+  template: `
+    <haind-workspace-nx-welcome></haind-workspace-nx-welcome>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'ng-conduit';
+}
