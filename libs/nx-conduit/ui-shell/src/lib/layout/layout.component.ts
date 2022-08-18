b5dd66412c33f153263d18abf8556b8b9d05edd6
haidnguyen <dinhhai281@gmail.com>
2022-08-18 17:47:20 +0700

    feat(nx-conduit): add footer

--- libs/nx-conduit/ui-shell/src/lib/layout/layout.component.ts
+++ libs/nx-conduit/ui-shell/src/lib/layout/layout.component.ts
@@ -2,14 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'haind-workspace-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
   template: `
     <haind-workspace-header></haind-workspace-header>
     <router-outlet></router-outlet>
+    <haind-workspace-footer></haind-workspace-footer>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
